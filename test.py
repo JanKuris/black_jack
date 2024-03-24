dbf0a7c84e335d89a24c954cdf1d37df88a58849
Jan Kuris <jan.kuris@gmail.com>
2024-03-24 18:33:44 +0100

    ..

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import random
-from replit import clear
 from art import logo 
+import os 
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def game():
     def card():
         cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -11,7 +15,7 @@ def game():
         if 11 in hand and sum(hand) > 21:
             hand.remove(11)
             hand.append(1)
-            
+
     player_hand = []
     player_hand.append(card())
     player_hand.append(card())
@@ -22,8 +26,8 @@ def game():
     print(f"Your card is {player_hand} current score is {sum(player_hand)}")
     print(f"Dealers first card: {dealer_hand[0]}")
 
-    black_jack = True
-    while black_jack == True:
+    game_over = True
+    while game_over == True:
         another_card = input("Do you want to get another card? Type 'y' or 'n': ").lower()
         if another_card == "y":
             player_hand.append(card())
@@ -38,53 +42,49 @@ def game():
                 dealer_hand.append(card())
                 adjust_ace(dealer_hand)  
                 dealer_value = sum(dealer_hand)
-        
             print(f"Player = {player_value}")
             print(f"Dealer = {dealer_value} and {dealer_hand}")
             
             if player_value == dealer_value:
                 print("Draw")
-                black_jack = False
+                game_over = False
             elif player_value == 21:
                 print("You win")
-                black_jack = False
-            elif dealer_value > 21 and player_value <= 22:
+                game_over = False
+            elif dealer_value > 21 and player_value <=  21:
                 print("You win")
-                black_jack = False
-            elif player_value >= 22 and dealer_value >= 22:
-                if dealer_value > player_value:
-                    print("Dealer wins")
-                    black_jack = False
-                elif dealer_value == player_value:
-                    print("Draw")
-                    black_jack = False
+                game_over = False
+            elif player_value > 21 and dealer_value > 21:
+                print("Dealer wins")
             elif player_value > 21 and dealer_value < 22:
                 print("Dealer wins")
-                black_jack = False
+                game_over = False
             elif player_value < 22 and dealer_value > 21:
                 print("You win")
-                black_jack = False
+                game_over = False
             elif player_value < dealer_value:
                 print("Dealer wins")
-                black_jack = False
+                game_over = False
             else:
                 print("You win")
-                black_jack = False
+                game_over = False
+        else:
+            print("you type wrong letter... please correct it!")
+            
 new_game = True
 while new_game:
-    new_game = input("Do you want to play new game? (y/n)").lower()
-    clear()
+    repeat = input("Do you want to play new game? (y/n)").lower()
+    cls()
     print(logo)
-    if new_game == 'y':
+    if repeat == 'y':
         game()
-        
-    else: 
+    elif repeat == "n":
         new_game = False
+    else: 
+        print("you type wrong letter... please correct it!")
 
 
 
 
 
         
-
-
