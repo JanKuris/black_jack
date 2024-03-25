5112e851749c8138d593170a65235ff0d359858b
Jan Kuris <jan.kuris@gmail.com>
2024-03-25 14:39:47 +0100

    final commit

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def deal_card():
-        cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-        return random.choice(cards)
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
 
 def adjust_ace(hand):
         if 11 in hand and sum(hand) > 21:
             hand.remove(11)
             hand.append(1)
 
-def print_score(player_hand, dealer_hand):
+def print_score(player_hand, dealer_hand): 
     print(f"Your cards: {player_hand}, current score: {sum(player_hand)}")
     print(f"Dealer's first card: {dealer_hand[0]}")
 
@@ -27,7 +27,7 @@ def determine_winner(player_hand, dealer_hand):
     elif dealer_value == 21 and len(dealer_hand) == 2:
          return "Dealer has a BLACK JACK!"
     elif player_value == 21 and len(player_hand) == 2:
-         return"Player have a BLACK JACK!"
+         return "Player have a BLACK JACK!"
     elif dealer_value > 21 or player_value > dealer_value:
         return "Player wins!"
     elif dealer_value > player_value:
@@ -40,7 +40,7 @@ def game():
     
     print_score(player_hand, dealer_hand)
 
-    while sum(player_hand) < 21:
+    while sum(player_hand) < 22:
         another_card = input("Do you want to get another card? Type 'y' or 'n': ").lower()
         
         if another_card == "y":
@@ -64,8 +64,13 @@ while play_again:
     print(logo)
     if repeat == 'y':
        game()
-    elif repeat != "y":
+    elif repeat == "n":
         play_again = False
+    else:
+        play_again = True
+
+#there is an issue with player black jack rule 
+        
 
 
         
